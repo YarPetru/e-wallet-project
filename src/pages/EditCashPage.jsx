953f22428aa0b8237c4b0210d8f76b5f9b74f0e4
Yaroslav Petrukhin <75678049+YarPetru@users.noreply.github.com>
2022-08-11 01:47:25 +0300

    feat: EditCashPage updated, provider and persistor in index.js added, styles updated

--- src/pages/EditCashPage.jsx
+++ src/pages/EditCashPage.jsx
@@ -1,3 +1,13 @@
-const EditCashPage = () => {};
+import EditCashContent from 'components/EditCashContent';
+import Title from 'components/Title';
+
+const EditCashPage = () => {
+  return (
+    <>
+      <Title title="Edit your cash" />
+      <EditCashContent />
+    </>
+  );
+};
 
 export default EditCashPage;
